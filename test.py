facfac24b18002e5c6664398ce638cf607cead72
Denys Vitali <denys.vitali@coelis.ch>
2017-06-09 23:26:20 +0200

    Add test.py

---
+++ test.py
@@ -0,0 +1,23 @@
+import moodle_cli
+import argparse
+import json
+import os
+
+from moodle_cli import utils
+from moodle_cli import logger
+
+BASE_URL = 'https://www2.icorsi.ch/'
+
+arg_parser = argparse.ArgumentParser(description='Downloads all course files from {0}'.format(BASE_URL))
+arg_parser.add_argument('base_dir', type=str, default=os.path.join(os.getcwd(), 'moodle_files'), nargs='?', help='download folder for the course files')
+arg_parser.add_argument('base_url', type=str, default=BASE_URL, nargs='?', help='base url of the moodle server')
+arg_parser.add_argument('-f', '--force', default=False, help='downloads files even when these already exists.', action='store_true')
+args = arg_parser.parse_args()
+try:
+        downloader = moodle_cli.MoodleCourseDownloader(save_dir=args.base_dir, base_url=args.base_url, keep_old_files=not args.force)
+        courses = downloader.extract_course_pages()
+        print(courses)
+        downloader.extract_and_download(courses)
+except Exception as error:
+        logger.exception("Failed to download course content: " + str(error))
+
